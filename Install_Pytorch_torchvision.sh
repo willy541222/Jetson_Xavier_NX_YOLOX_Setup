456f0250451bfe528f034d08524151a8ec947aff
willy541222 <willy541222@gmail.com>
2022-04-30 03:46:29 +0800

    Fix:Install_Pytorch_torchvision.sh file.

--- Install_Pytorch_torchvision.sh
+++ Install_Pytorch_torchvision.sh
@@ -4,8 +4,8 @@ cd ..
 
 wget https://nvidia.box.com/shared/static/cs3xn3td6sfgtene6jdvsxlr366m2dhq.whl -O torch-1.7.0-cp36-cp36m-linux_aarch64.whl
 sudo apt-get install python3-pip libopenblas-base libopenmpi-dev
-pip3 install Cython
-pip3 install numpy torch-1.7.0-cp36-cp36m-linux_aarch64.whl
+sudo pip3 install Cython
+sudo pip3 install numpy torch-1.7.0-cp36-cp36m-linux_aarch64.whl
 sudo apt-get install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
 
 
