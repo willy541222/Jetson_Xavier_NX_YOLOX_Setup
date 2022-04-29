5dac78513c14d9070f33501a98425eeb59df2de6
willy541222 <willy541222@gmail.com>
2022-04-29 20:44:50 +0800

    Fix:Setup_VNC.sh file

--- Setup_VNC.sh
+++ Setup_VNC.sh
@@ -12,15 +12,15 @@ sudo ln -s ../vino-server.service   /usr/lib/systemd/user/graphical-session.targ
 gsettings set org.gnome.Vino prompt-enabled false
 gsettings set org.gnome.Vino require-encryption false
 
-cp -f BASEDIR/org.gnome.Vino.gschema.xml /usr/share/glib-2.0/schemas/org.gnome.Vino.gschema.xml
+cp -f $BASEDIR/org.gnome.Vino.gschema.xml /usr/share/glib-2.0/schemas/org.gnome.Vino.gschema.xml
 
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas
 gsettings set org.gnome.Vino enabled true 
 mkdir -p ~/.config/autostart
 
-cp -f BASEDIR/vino-server.desktop ~/.config/autostart/vino-server.desktop
+cp -f $BASEDIR/vino-server.desktop ~/.config/autostart/vino-server.desktop
 
-cp -f BASEDIR/xorg.conf /etc/X11/xorg.conf
+cp -f $BASEDIR/xorg.conf /etc/X11/xorg.conf
 
 /usr/lib/vino/vino-server
 
